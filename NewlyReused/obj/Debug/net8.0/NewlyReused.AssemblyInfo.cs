594c477bf6a5dcdf4bbf51dc88c4a1eb8b5a8b70
Marie Bryjova <xbryjova@fi.muni.cz>
2024-06-13 16:20:44 +0200

    mapa s fungujícíma popiskama

--- NewlyReused/obj/Debug/net8.0/NewlyReused.AssemblyInfo.cs
+++ NewlyReused/obj/Debug/net8.0/NewlyReused.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06bcff481a55fbe66daf8d54c68b0fc212ef2766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30801407b53a70404e1bfea47dab9aee7061d56a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
