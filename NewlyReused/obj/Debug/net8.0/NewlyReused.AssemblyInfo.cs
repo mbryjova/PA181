f7372f44aac6037c0cd1c05eb3671e9575b6111d
xkubick1 <83333851+xkubick1@users.noreply.github.com>
2024-06-15 19:28:05 +0200

    Spustitelná verze - data se berou z hardcoded stringu

--- NewlyReused/obj/Debug/net8.0/NewlyReused.AssemblyInfo.cs
+++ NewlyReused/obj/Debug/net8.0/NewlyReused.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd40760a9f3b69e5bc9edd9ed8a5cd56955c0da5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af83b2201732d49ffc3043b730d54a74e9308b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
