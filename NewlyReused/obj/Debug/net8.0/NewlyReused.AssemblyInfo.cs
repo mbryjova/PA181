ebc2e1a3f5882c6105c2d241e0bafcb6aa349977
Marie Bryjova <xbryjova@fi.muni.cz>
2024-06-16 18:53:14 +0200

    privacy + zmena id

--- NewlyReused/obj/Debug/net8.0/NewlyReused.AssemblyInfo.cs
+++ NewlyReused/obj/Debug/net8.0/NewlyReused.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379819a518fd778bf54fc9881b62e09826454c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105e22bfe588c905b24526ac4db242770fcdaf6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewlyReused")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
